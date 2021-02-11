a7417dfa1ac57d4d5e8a591f37e45ef575e69351
tabishhameed <tabishhameed34@gmail.com>
2021-02-12 07:22:55 +0800

    some changing in database , app and store a data in json file

--- utlis/database.py
+++ utlis/database.py
@@ -1,34 +1,41 @@
+import json
 
-books_file = 'books.txt'
+"""
+[
+{
+'name': 'tab',
+'author': 'ham',
+'read': True
+}
+]
+"""
 
-def create_table():
-    with open(books_file, 'w'):
-        pass
+books_file = 'books.json'
+
+def create_book_table():
+    with open(books_file, 'w') as file:
+        json.dump([], file)
 
 def add_book(name, author):
-   with open(books_file, 'a') as file:
-       file.write(f'{name}, {author}, 0\n')
+   books = get_all_books()
+   books.append ({'name': name, 'author': author, 'read': False})
+   _save_all_books(books)
 
 def get_all_books():
     with open(books_file, 'r') as file:
-        lines = [line.strip().split(',') for line in file.readlines()]
-    return [
-        {'name': line[0], 'author': line[2], 'read': line[1]}
-        for line in lines
-    ]
+       return json.load(file)
 
 def mark_book_read(name):
     books = get_all_books()
 
     for book in books:
         if book['name'] == name:
-            book['read'] = '1'
+            book['read'] = True
     _save_all_books(books)
 
 def _save_all_books(books):
     with open(books_file, 'w') as file:
-        for book in books:
-            file.write(f"{book['name']}, {book['author']}, {book['read']}\n")
+       return json.dump(books, file)
 
 
 
