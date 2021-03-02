d7453e898c6b84f313c11505a41afdc50f60640f
tabishhameed <tabishhameed34@gmail.com>
2021-03-02 21:39:11 +0800

    done with context manager

--- utlis/database.py
+++ utlis/database.py
@@ -1,4 +1,4 @@
-import sqlite3
+from database_connection import Databaseconnection
 
 """
 [
@@ -13,40 +13,41 @@ import sqlite3
 books_file = 'books.json'
 
 def create_book_table():
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
+        with Databaseconnection ('data.db') as connection:
+          cursor = connection.cursor()
 
-        cursor.execute('CREATE TABLE books(name text primary key, author text, read integer)')
-        connection.commit()
+          cursor.execute('CREATE TABLE IF NOT EXISTS books (name text primary key, author text, read integer)')
 
-        connection.close()
 
 def add_book(name, author):
-   books = get_all_books()
-   books.append ({'name': name, 'author': author, 'read': False})
-   _save_all_books(books)
+    with Databaseconnection ('data.db') as connection:
+       cursor = connection.cursor()
+
+       cursor.execute('INSERT INTO books VALUES (?, ?, 0)', (name,author))
 
 def get_all_books():
-    with open(books_file, 'r') as file:
-       return json.load(file)
+    with Databaseconnection ('data.db') as connection:
+      cursor = connection.cursor()
+      cursor.execute('SELECT * FROM books')
+      books = [{'name': row[0],'author': row[1], 'read': row[2]}for row in cursor.fetchall()]
+
+    return books
 
 def mark_book_read(name):
-    books = get_all_books()
+   with Databaseconnection ('data.db') as connection:
+      cursor = connection.cursor()
 
-    for book in books:
-        if book['name'] == name:
-            book['read'] = True
-    _save_all_books(books)
+      cursor.execute('UPDATE books SET read=1 WHERE name=?', (name,))
 
-def _save_all_books(books):
-    with open(books_file, 'w') as file:
-       return json.dump(books, file)
+
 
 
 
 
 def delete_book(name):
-    books = get_all_books()
-    books = [book for book in books if book ['name'] != name]
-    _save_all_books(books)
+    with Databaseconnection ('data.db') as connection:
+       cursor = connection.cursor()
+       cursor.execute('DELETE FROM books WHERE name=?', {name})
+
+
 
